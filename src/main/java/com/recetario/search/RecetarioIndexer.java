2be300877632b314e90185dd4b0576b4b12b57f9
Gregory Valderrama <gvalderrama@verizon.com>
2015-11-23 16:41:45 -0500

    add clases

--- src/main/java/com/recetario/search/RecetarioIndexer.java
+++ src/main/java/com/recetario/search/RecetarioIndexer.java
@@ -5,8 +5,16 @@
  */
 package com.recetario.search;
 
+import com.hp.hpl.jena.shared.NotFoundException;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
-import java.io.IOException; 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import jdk.nashorn.internal.objects.NativeArray;
 import org.apache.lucene.analysis.es.SpanishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -23,25 +31,31 @@ import org.apache.lucene.util.Version;
  * @author gregory
  */
 public class RecetarioIndexer {
-    private final String nmCarpetaIndice = "T://Cursos//Ingeniería y Gestión del Conocimiento 2015-2//work//indiceT";
+    private String FileDirectory = "fileDirectory";
+    private String IndexDirectory = "indexDirectory";
     private Directory Directory;
     //private StandardAnalyzer analizador;
-    private SpanishAnalyzer Analyzer;
+    private StandardAnalyzer Analyzer;
     private Version version;
     private IndexWriterConfig config;
     private IndexWriter Writter;
     
+    public RecetarioIndexer(String FileDir, String IndexDir)
+    {
+        this.FileDirectory  = FileDir; 
+        this.IndexDirectory = IndexDir;  
+    }
+    public void ClearDocuments() throws IOException
+    {
+        this.Writter.deleteAll();
+    }
     public void CreateWriter() throws IOException
     {
-        //this.directorio = FSDirectory.open(new File(this.nmCarpetaIndice));
-        this.Directory =new RAMDirectory();  
-        
-        this.Analyzer = new SpanishAnalyzer(); 
-        
-        this.version = Version.LUCENE_4_10_1;
-        
-        this.config =  new IndexWriterConfig(this.version, this.Analyzer); 
-        
+        this.Directory = FSDirectory.open(new File(this.IndexDirectory));
+        //this.Directory =new RAMDirectory();       
+        this.Analyzer = new StandardAnalyzer();        
+        this.version = Version.LUCENE_4_10_1;       
+        this.config =  new IndexWriterConfig(this.version, this.Analyzer);      
         this.Writter = new IndexWriter(this.Directory, this.config); 
     }
     
@@ -56,7 +70,51 @@ public class RecetarioIndexer {
         Field fieldTexto = new Field("texto", texto, TextField.TYPE_STORED );
         Field fieldClase = new Field("clase",  clase,  TextField.TYPE_STORED); 
         document.add(fieldTexto);
-        
+        document.add(fieldClase);
         this.Writter.addDocument(document);  
+    }
+    private String ReadText(String Path) throws NotFoundException, IOException
+    {
+        FileInputStream fstream = new FileInputStream(Path); 
+        DataInputStream entrada = new DataInputStream(fstream);
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+        String strLinea;
+        String texto = "";
+        while ((strLinea = buffer.readLine()) != null) {
+            texto = texto + strLinea + "\n";
+        }
+        return texto;
+    }
+    private String[] ReadClases(String Path) throws FileNotFoundException, IOException
+    {
+        
+        ArrayList<String> al = new ArrayList<>();         
+        FileInputStream fstream = new FileInputStream(Path); 
+        DataInputStream entrada = new DataInputStream(fstream);
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+        String strLinea;
+  
+        while ((strLinea = buffer.readLine()) != null) {
+            
+            al.add(strLinea);
+
+        }
+        
+        return al.toArray(new String[al.size()]);
+    }
+    public void Load() throws IOException
+    {
+        this.CreateWriter(); 
+        this.ClearDocuments(); 
+        for (int i = 1; i < 13; i++) {
+            String texto = ReadText("fileDirectory/Texto" + i +  ".txt");
+            String[] clases = ReadClases("fileDirectory/Clase" + i + ".txt");
+          
+            for(String clase:clases)
+            {
+                this.Index(texto, clase);
+            }
+        }
+        this.CloseWritter();
     }
 }
---
+++ src/main/java/com/recetario/search/RecetarioSearcher.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.recetario.search;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+/**
+ *
+ * @author gregory
+ */
+public class RecetarioSearcher {
+    
+    private String IndexDirectory;  
+    private Directory directory;
+    private DirectoryReader directoryReader;
+    private StandardAnalyzer analyzer;
+    private IndexSearcher searcher;
+
+    public RecetarioSearcher(String IndexDir) throws IOException
+    {
+        this.IndexDirectory = IndexDir;
+       
+        
+    }
+    
+    public void Close() throws IOException
+    {
+        this.directoryReader.close();
+    }
+    public void Open() throws IOException
+    {
+         this.directory = FSDirectory.open(new File(this.IndexDirectory));
+        
+        this.directoryReader = DirectoryReader.open(directory);
+               
+        this.searcher = new IndexSearcher(this.directoryReader);
+        
+        this.analyzer = new StandardAnalyzer();
+        
+        this.directory.close();
+    }
+    public String[] Search(String clase) throws ParseException, IOException
+    {
+        ScoreDoc[] listaDocs;
+        QueryParser parser  = new QueryParser("clase", this.analyzer); 
+        Query query = parser.parse(clase);
+               
+        ArrayList<String> al = new ArrayList<>(); 
+        
+        listaDocs = this.searcher.search(query, 100).scoreDocs;
+        
+        for(ScoreDoc sdoc:listaDocs)
+        {
+            Document document = this.searcher.doc(sdoc.doc);
+            String text = document.get("texto");
+            al.add(text);         
+        }
+        return al.toArray(new String[al.size()]);
+    }   
+    public Map<String, String[]> Search(ArrayList<String> clases) throws ParseException, IOException
+    {
+        Map<String,String[]> results = new HashMap<>();  
+        for(String clase:clases)
+        {
+            String[] result = this.Search(clase);
+            
+            results.put(clase, result);
+            
+        }
+        return results;
+    }
+}
